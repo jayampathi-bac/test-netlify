e281589a8c68aa17a299b645c02ef26923a271ea
jayampathi-bac <jayampathi.bac@gmail.com>
2022-04-29 01:59:44 +0530

    added scrapping

--- functions/scrap-background.js
+++ functions/scrap-background.js
@@ -4,8 +4,8 @@ const axios = require("axios");
 exports.handler = async function (event) {
     const {email, url_x, url_y} = JSON.parse(event.body);
 
-    // const scrappedDataResponse = await axios.post(`https://vue-test-22.netlify.app/.netlify/functions/pup-core`, {
-    const scrappedDataResponse = await axios.post(`http://localhost:9664/.netlify/functions/pup-core`, {
+    // const scrappedDataResponse = await axios.post(`http://localhost:9664/.netlify/functions/pup-core`, {
+    const scrappedDataResponse = await axios.post(`https://vue-test-22.netlify.app/.netlify/functions/pup-core`, {
         url_x : url_x,
         url_y : url_y,
         email : email
